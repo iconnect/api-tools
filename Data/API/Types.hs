0d0525a7604f58d338db5af97c4b3f850bdb2606
Chris Dornan <chris@chrisdornan.com>
2013-05-29 09:12:42 +0100

    extend date parser (seconds now optionsl)

--- Data/API/Types.hs
+++ Data/API/Types.hs
@@ -170,6 +170,7 @@ utcFormats :: [String]
 utcFormats =
                         [ "%Y-%m-%dT%H:%M:%S%z"
                         , "%Y-%m-%dT%H:%M:%S%Z"
+                        , "%Y-%m-%dT%H:%S%Z"
                         , utcFormat
                         ]
 
