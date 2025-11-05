322090045c9af07613fa78b16b5c1edf8f789665
Adam Gundry <adam@well-typed.com>
2025-11-05 09:30:18 +0000

    Simpler shrinkRecord

--- src/Data/API/Tools/QuickCheck.hs
+++ src/Data/API/Tools/QuickCheck.hs
@@ -18,7 +18,6 @@ import           Data.Time
 import           Language.Haskell.TH
 import           Prelude
 import           Test.QuickCheck                as QC
-import qualified Data.List as L
 
 -- | Tool to generate 'Arbitrary' instances for generated types. This tool generates
 -- also a stock shrinker via the 'generic-arbitrary' package, which means we require
@@ -94,42 +93,60 @@ gen_sr_ab = mkTool $ \ ts (an, sr) -> mkArbitraryInstance ts (nodeRepT an) (bdy 
     -- to generate a list of lists, each sublist being the shrinking of a single
     -- individual field, and finally mconcat everything together.
     -- Example:
-    -- shrink = \(Foo a b c) ->
-    --  (Foo <$> shrink a <*> pure b   <*> pure c) ++
-    --  (Foo <$> pure   a <*> shrink b <*> pure c) ++
-    --  (Foo <$> pure   a <*> pure b   <*> shrink c)
+    --
+    -- shrink = \ x ->
+    --   case x of
+    --     Foo a b c ->
+    --       concat [ Foo <$> shrink a <*> pure b   <*> pure c
+    --              , Foo <$> pure   a <*> shrink b <*> pure c
+    --              , Foo <$> pure   a <*> pure b   <*> shrink c
+    --              ]
+    --
     shrinkRecord :: APINode -> SpecRecord -> ExpQ
     shrinkRecord an sr = do
-      x <- newName "x"
-      -- Matches the fields of the record with fresh variables
-      -- [( "field1", "field1"), ("field2", "field2") ... ]
-      recordPatterns <-
-        forM (srFields sr) $ \(fn,_) -> do
-          let freshRecName = pref_field_nm an fn
-          freshPatName <- nodeFieldP an fn
-          pure (freshRecName,freshPatName)
+      -- List of field names in the record
+      let fields :: [Name]
+          fields = map (pref_field_nm an . fst) (srFields sr)
 
+      -- Given a list of fields with a distinguished element, construct
+      --    Foo <$> pure x0 <*> ... <*> shrink xM <*> ... <*> pure xN
+      -- where the boolean indicates which field should use 'shrink'.
+      let shrinkMarkedField :: [(Bool, Name)] -> ExpQ
+          shrinkMarkedField flds =
+              applicativeE (nodeConE an) $
+                  flip map flds $ \(shrunk, fld) ->
+                      if shrunk then [e| QC.shrink $(varE fld) |]
+                                else [e| pure      $(varE fld) |]
+
+      -- Construct the list
+      --   [ Foo <$> shrink a <*> pure b   <*> ...
+      --   , Foo <$> pure   a <*> shrink b <*> ...
+      --   , ...
+      --   ]
+      let shrinkAllFields :: ExpQ
+          shrinkAllFields = listE (map shrinkMarkedField (distinguishedElements fields))
+
+      x <- newName "x"
       lamE [varP x] $
         caseE (varE x) [
-          -- mighty Universe, forgive me for this ghastly code.
-          match (recP nm (map pure recordPatterns))
-                (normalB $ concatParts $
-                  flip map recordPatterns $ \(target, _) ->
-                    let targetTxt = show target
-                    in appE (varE 'L.singleton) $
-                         applicativeE (nodeConE an) $
-                           flip map recordPatterns $ \(fld, _) ->
-                             let fldTxt = show fld
-                             in if fldTxt == targetTxt
-                                   then [e| QC.shrink $(varE target) |]
-                                   else [e| pure $(varE fld) |]
-                ) []
+          -- Foo a b c -> concat [...]
+          match (recP nm (map (\n -> fieldPat n (varP n)) fields))
+                (normalB [e| concat $shrinkAllFields |])
+                []
         ]
        where
          nm = rep_type_nm an
 
-concatParts :: [Q Exp] -> Q Exp
-concatParts parts = [| join $ join $ $(listE parts) |]
+-- | Turn an N-element list into N lists of N pairs, each of which has a single
+-- distinguished element marked True.
+--
+-- >>> distinguishedElements "abc"
+-- [[(True,'a'),(False,'b'),(False,'c')],[(False,'a'),(True,'b'),(False,'c')],[(False,'a'),(False,'b'),(True,'c')]]
+--
+distinguishedElements :: [a] -> [[(Bool, a)]]
+distinguishedElements []     = []
+distinguishedElements (x:xs) = ((True, x) : map ((,) False) xs)
+                             : map ((False, x) :) (distinguishedElements xs)
 
 -- | Generate an 'Arbitrary' instance for a union:
 --
